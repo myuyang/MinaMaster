378ef36606525fe8201706c2967724233506b2fb
myuyang <myuyang@qq.com>
2019-07-03 16:41:04 +0800

    first

---
+++ HeartBeatClient/app/src/main/java/com/example/heartbeatclient/DataConfig.java
@@ -0,0 +1,17 @@
+package com.example.heartbeatclient;
+
+public class DataConfig {
+    //public static String IP="192.168.155.1";            //开WiFi共享大师
+    public static String IP="192.168.31.219";            //不开WiFi共享大师
+    public static int Port=8234;
+
+    /**
+     * Socket配置
+     */
+    public static final int PORT = 33000;
+    public static final int CONNECT_TIMEOUT = 3000;
+    public static final int CONNECT_TIME = 3000;
+    public static final int READ_BUFFER_SIZE = 2048;
+
+    public static final int SENT_HEADER = 65512;
+}
